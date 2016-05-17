3987cbb71d6d1e4c8d8203d734995fd89162c121
Jordan LaShomb <jtlashomb9@gmail.com>
2016-05-16 23:26:48 -0400

    Timing for the gameObject file

--- src/netOrcsShared/GameObjects.java
+++ src/netOrcsShared/GameObjects.java
@@ -3,12 +3,14 @@ package netOrcsShared;
 import java.awt.Color;
 import java.awt.Point;
 import java.io.Serializable;
+import java.sql.Time;
 
 public abstract class GameObjects implements Serializable{
 	
 	private boolean isAlive = true;
 	int speed;
 	Color color;
+	private Time timeAlive;
 	abstract public Point getPosition();
 	abstract public void setPosition(Point position);
 	abstract public int getIndex();
@@ -18,6 +20,12 @@ public abstract class GameObjects implements Serializable{
 	abstract public void setColor(Color color);
 	abstract public void setSpeed(int speed);
 	abstract public int getSpeed();
+	public void setTimeAlive(Time time){
+		this.timeAlive = time;
+	}
+	public Time getTime(){
+		return this.timeAlive;
+	}
 	public GameObjects(){
 		speed=0;
 	}
